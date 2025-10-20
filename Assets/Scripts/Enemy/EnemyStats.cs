d4386f31007bbbf89102d1b890fd170a571c7ed5
sirclex <lethanhluan2000@gmail.com>
2025-10-20 18:38:30 +0700

    Feat: Random enemies spawner

--- Assets/Scripts/Enemy/EnemyStats.cs
+++ Assets/Scripts/Enemy/EnemyStats.cs
@@ -5,15 +5,36 @@ public class EnemyStats : MonoBehaviour
     public EnemyScriptableObject enemyData;
 
     // Current stats
+    [HideInInspector]
     float currentMoveSpeed;
+
+    [HideInInspector]
     float currentHealth;
+
+    [HideInInspector]
     float currentDamage;
+
+    public float despawnDistance = 30f;
+    Transform player;
 
     void Awake()
     {
         currentMoveSpeed = enemyData.MoveSpeed;
         currentHealth = enemyData.MaxHealth;
         currentDamage = enemyData.Damage;
+    }
+
+    void Start()
+    {
+        player = FindFirstObjectByType<PlayerStats>().transform;
+    }
+
+    void Update()
+    {
+        if (Vector2.Distance(transform.position, player.position) >= despawnDistance)
+        {
+            ReturnEnemy();
+        }
     }
 
     public void TakeDamage(float damage)
@@ -37,5 +58,20 @@ public class EnemyStats : MonoBehaviour
             PlayerStats player = collision.gameObject.GetComponent<PlayerStats>();
             player.TakeDamage(currentDamage);
         }
+    }
+
+    private void OnDestroy()
+    {
+        EnemySpawner spawner = FindFirstObjectByType<EnemySpawner>();
+        if (spawner != null)
+        {
+            spawner.OnEnemyKilled();
+        }
+    }
+
+    private void ReturnEnemy()
+    {
+        EnemySpawner spawner = FindFirstObjectByType<EnemySpawner>();
+        transform.position = player.position + spawner.relativeSpawnPoints[Random.Range(0, spawner.relativeSpawnPoints.Count)].position;
     }
 }
