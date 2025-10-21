03dade5d61126f081910b896fd4da1bb7cecb845
sirclex <lethanhluan2000@gmail.com>
2025-10-22 00:47:39 +0700

    feat: bobbing animation

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -212,7 +212,7 @@ public class GameManager : MonoBehaviour
 
         if (stopwatchTime >= timeLimit)
         {
-            GameOver();
+            playerObject.SendMessage("Kill");
         }
     }
 
